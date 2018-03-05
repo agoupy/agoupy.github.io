dfc7ae544de39757fd28c3da4e90ba4fb325f339
agoupy <alexandre.goupy@gmail.com>
2018-03-05 17:17:25 +0100

    Change Codes page in Misc. one

--- update.sh
+++ update.sh
@@ -1,1 +1,5 @@
+## Used to change things in several files (update date for example)
+
 sed -i 's/Last update: 02\/2018/Last update: 03\/2018/g' *.html
+
+#sed -i 's/"codes.html">Codes/"misc.html">Misc./g' *.html
