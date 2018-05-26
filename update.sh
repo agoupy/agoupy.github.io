f2fef0ac903b173ee187e067239823f6f5c37b46
agoupy <alexandre.goupy@gmail.com>
2018-05-26 13:55:40 +0200

    Change header

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 ## Used to change things in several files (update date for example)
 
-sed -i 's/Last update: 03\/2018/Last update: 05\/2018/g' *.html
+#sed -i 's/Last update: 03\/2018/Last update: 05\/2018/g' *.html
 
-#sed -i 's/<title>A. Goupy<\/title>/<title>A. Goupy | PhD Student<\/title>/g' *.html
+sed -i 's/<title>A. Goupy | PhD Student<\/title>/<title>A. Goupy | Applied Mathematics<\/title>/g' *.html
