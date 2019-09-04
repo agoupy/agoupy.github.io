b84e23c20a5533774bfa34cf338b51bf61dc115c
goupya <goupya@u-un00301910>
2019-09-04 16:38:13 +0200

    Change title with complete firstname

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 ## Used to change things in several files (update date for example)
 
-sed -i 's/Last update: 04\/2019/Last update: 07\/2019/g' *.html
+#sed -i 's/Last update: 04\/2019/Last update: 07\/2019/g' *.html
+
+sed -i 's/>A. Goupy</>Alexandre Goupy</g' *.html
 
 #sed -i 's/<!--<li><a href="teaching.html">Teaching<\/a><\/li>-->/<li><a href="teaching.html">Teaching<\/a><\/li>/g' *.html
