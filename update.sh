2da192bdd565074ec00a3bde3543b8bfd11a72f4
ALEXANDRE GOUPY <alexandre.goupy@edf.fr>
2023-03-20 15:59:55 +0100

    Change update date

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 ## Used to change things in several files (update date for example)
 
-#sed -i 's/Last update: 04\/2019/Last update: 07\/2019/g' *.html
+sed -i 's/Last update: 05\/2022/Last update: 03\/2023/g' *.html
 
 #sed -i 's/cmla.ens-paris-saclay.fr/edf.fr/g' *.html
 
-sed -i 's/<li><a href="misc.html">Misc.<\/a><\/li>/<!--<li><a href="misc.html">Misc.<\/a><\/li>-->/g' *.html
+#sed -i 's/<li><a href="misc.html">Misc.<\/a><\/li>/<!--<li><a href="misc.html">Misc.<\/a><\/li>-->/g' *.html
