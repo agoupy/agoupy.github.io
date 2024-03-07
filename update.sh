8280e29a98c7171660a9813457750008e0cf581d
ALEXANDRE GOUPY <alexandre.goupy@edf.fr>
2024-03-07 14:17:45 +0100

    update date and copyright

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 ## Used to change things in several files (update date for example)
 
-sed -i 's/Last update: 05\/2022/Last update: 03\/2023/g' *.html
+sed -i 's/Last update: 03\/2023/Last update: 07\/2024/g' *.html
+
+sed -i 's/<li>&copy; 2022, Alexandre/<li>&copy; 2024, Alexandre/g' *.html
 
 #sed -i 's/cmla.ens-paris-saclay.fr/edf.fr/g' *.html
 
