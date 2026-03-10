b6d8e4794c084e189124ac3605dc90cbc865b56f
ALEXANDRE GOUPY <alexandre.goupy@edf.fr>
2026-03-10 11:07:15 +0100

    Update dates de mise à jour et suppression page recherche

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 ## Used to change things in several files (update date for example)
 
-sed -i 's/Last update: 03\/2023/Last update: 07\/2024/g' *.html
+sed -i 's/Last update: 03\/2024/Last update: 03\/2026/g' *.html
 
-sed -i 's/<li>&copy; 2022, Alexandre/<li>&copy; 2024, Alexandre/g' *.html
+#sed -i 's/<li>&copy; 2024, Alexandre/<li>&copy; 2026, Alexandre/g' *.html
 
 #sed -i 's/cmla.ens-paris-saclay.fr/edf.fr/g' *.html
 
-#sed -i 's/<li><a href="misc.html">Misc.<\/a><\/li>/<!--<li><a href="misc.html">Misc.<\/a><\/li>-->/g' *.html
+#sed -i 's/<li><a href="research.html">Research<\/a><\/li>/<!--<li><a href="research.html">Research<\/a><\/li>-->/g' *.html
